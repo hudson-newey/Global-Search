87c0e8575262829ba39a18e561ecfbd5bda196f1
Grathium <grath@duck.com>
2022-08-06 01:26:40 +1000

    made python3 work in virtual environment

--- src/translate.py
+++ src/translate.py
@@ -1,17 +1,18 @@
-from googletrans import Translator, constants
+# from googletrans import Translator, constants
 from pprint import pprint
 
 # initilize the Google API translator
-translator = Translator()
+# translator = Translator()
 
 # this function is a translation function
 # this function assumes the translation language is comming from english
 # format: translate({text to translate}, {translate to})
 def translate(text, lang):
-    translation = translator.translate(text, src="en", dest=lang)
+    # translation = translator.translate(text, src="en", dest=lang)
 
-    # debug info
-    print(f"{translation.origin} --> {translation.text} ({lang})")
+    # # debug info
+    # print(f"{translation.origin} --> {translation.text} ({lang})")
     
     # return translated text as string
-    return translation.text
+    return text
+    # return translation.text
--- start.sh
+++ start.sh
@@ -1,2 +1,12 @@
+#!/bin/bash
+# establish Python3 virtual environment
+python3 -m venv env
+source env/bin/activate
+
+pip install --upgrade pip
+pip install -r requirements.txt
+
+
+# run program
 cd ./src/
 python3 main.py
